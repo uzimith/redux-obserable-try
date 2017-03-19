4c3c86bced3252703c7997b52d06d3f8d81cf126
uzimith <uzimith.x9@gmail.com>
2017-03-19 17:33:41 +0900

    add mytypes

---
+++ typings/mytypes.d.ts
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import { ActionsObservable } from "redux-observable";
+import { MiddlewareAPI } from "redux";
+import { Observable } from "rxjs/Observable";
+
+declare module "react-redux" {
+    export function connect<TStateProps, TOwnProps>(
+        mapStateToProps?: MapStateToProps<Partial<TStateProps>, TOwnProps>,
+        mapDispatchToProps?:
+            MapDispatchToPropsFunction<Partial<TStateProps>, TOwnProps> | Partial<TStateProps>,
+        mergeProps?: MergeProps<Partial<TStateProps>, Partial<TStateProps>, TOwnProps>,
+        options?: Options
+    ): ComponentDecorator<TStateProps, TOwnProps>;
+}
+
+declare module "redux-observable" {
+    export interface Epic<T, S> {
+        (action$: ActionsObservable<T>, store: MiddlewareAPI<S>): Observable<any>;
+    }
+}
+
+declare module "react-router/lib/Route" {
+    export interface RouteProps {
+        exact?: boolean;
+        component: ({ }) => JSX.Element;
+    }
+}
+
+declare module "react-router-redux" {
+    export class ConnectedRouter extends Component<{
+        history: {};
+    }, any> {
+        render(): JSX.Element;
+    }
+    export interface RouterState {
+        location: {
+            pathname: string;
+            search: string;
+            hash: string;
+        };
+    }
+}
